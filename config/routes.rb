0f7d714105d67422dc3383b9a3b7fd77f5da21ce
Swetha-saravanan <swethakarthika16@gmail.com>
2022-08-17 09:19:19 +0530

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'home#home_design'
+  root 'users#login'
   get 'play/display'
   get 'reports/display_reports'
   get 'assessments/display'
