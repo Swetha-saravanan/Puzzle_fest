35693d2213166a3529e7179111d9e5ef581f33ff
Swetha-saravanan <swethakarthika16@gmail.com>
2022-08-22 22:46:26 +0530

    routes changed

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -2,6 +2,6 @@
 
 class Assessment < ApplicationRecord
   has_one_attached :images
-  validates :name, presence: true, length: { minimum: 3, maximum: 100 }, format: { with: /\A[^0-9`!@#$%\^&*+_=]+\z/ },
+  validates :name, presence: true, length: { minimum: 3, maximum: 100 }, format: { with: /\A[^0-9`!@#$%\^&*+_=]+\z/ }
                   #  uniqueness: { case_senitive: false }
 end
